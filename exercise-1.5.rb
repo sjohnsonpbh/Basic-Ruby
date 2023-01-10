0ec1aab18e1ff8b5cd29570eccd950d1376312bc
sjohnsonpbh <47424009+sjohnsonpbh@users.noreply.github.com>
2023-01-10 13:52:20 -0600

    Create exercise-1.5.rb

---
+++ exercise-1.5.rb
@@ -0,0 +1,26 @@
+Exercise 1.5: Printing user data
+Create a Ruby file called exercise-1.5.rb.
+
+Given the array of hashes:
+
+users = [
+    {
+        name: "John Doe",
+        age: 43
+    },
+    {
+        name: "Amy Singer",
+        age: 53
+    },
+    {
+        name: "Jimmy Lendricks",
+        age: 23
+    }
+]
+Use a while loop to print the values of each hash such as "My name is ... and I am ...". If the first name starts with "Jimmy", print out "My name and age is confidential."
+
+expected output:
+
+> My name is John Doe and I am 43.
+> My name is Amy Singer and I am 54.
+> My name and age is confidential.
