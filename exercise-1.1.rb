c24d0d1de2ff83d79ef6fd63c2eeb0a83062d3e9
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-01-10 18:31:37 +0000

    Completed exercise 1.1

---
+++ exercise-1.1.rb
@@ -0,0 +1,20 @@
+# Exercise 1.1: Print to the console
+
+# Create a file named exercise-1.1.rb and use the p method to print the string "p" to the console.
+p "p"
+
+# Use the puts method to print the string "puts" to the console.
+puts "puts"
+# Use the print method to print the string "print" to the console.
+print "print" 
+
+# Declare a variable called my_name and assign it your name. Print the value of my_name to the console.
+my_name = "Scott"
+puts "#{my_name}"
+
+
+# Declare a variable called beginning_sentence and assign it the string "My name is ". Declare another variable called full_sentence and assign it the combination of beginning_sentence and my_name using the + operator. Print full_sentence to the console.
+
+beginning_sentence = "My name is "
+puts full_sentence = "#{beginning_sentence}" + "#{my_name}"
+
