4166c1fcbc98432407b57364d786c39c5e814b9e
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-01-12 17:51:32 +0000

    Created a class folder structure. Moved files accordingly.

---
+++ class_02/exercise-1.6.rb
@@ -0,0 +1,16 @@
+# Exercise 1.6: Nested Loops
+# Create a Ruby file called exercise-1.6.rb.
+
+# Given the value:
+
+# [[1,2,3],[[[4,5,6]]]]
+# Print each number in order.
+
+# expected output:
+
+# > 1
+# > 2
+# > 3
+# > 4
+# > 5
+# > 6
