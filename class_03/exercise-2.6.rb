1b3588aa2d688ea6c2e2f0b5520bc575aee9121e
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-01-13 12:06:01 -0600

    completed exercise 2.6

--- class_03/exercise-2.6.rb
+++ class_03/exercise-2.6.rb
@@ -9,6 +9,18 @@
 
 # puts [].methods # Ruby Remember me - how to find all ruby build in array methods
 
+# NOTES Ruby Remember me - Most Common Ruby Array Methods You Should Know
+# ++++++++++++++++++++++++++++
+# .length. The .length method tallies the number of elements in your array and returns the count: array. ...
+# .first. The .first method accesses the first element of the array, the element at index 0: array. ...
+# .last. ...
+# .take. ...
+# .drop. ...
+# array index. ...
+# .pop. ...
+# .shift.
+
+
 names_list =[ 
   {
     name: "Amy"
@@ -18,9 +30,31 @@ names_list =[
   },
   {
     name: "Johnny"
-  },
+  }
 ]
 
-puts names_list
+names_list.each {|the_name| puts "Hello, #{the_name[:name]}"}
 
-puts "First name in list is " + names_list.first
+# using .first array method
+ puts "First name is #{names_list.first}"
+
+# array.length
+puts "To Start, there are #{names_list.length} names in the array."
+
+# array.push - adds to the end of the array
+names_list.push(name: "Nolan")
+ puts "Pushed Nolan, There are #{names_list.length} names in the array."
+
+# array.unshift - adds to the beginning of the array
+names_list.unshift(name: "Scott")
+puts "Unshifted Scott There are #{names_list.length} names in the array."
+
+
+
+# array.delete - removes an element permanently
+# Code Coach - why does this delete not work?
+names_list.delete(1)
+ puts "Deleted Scott, There are #{names_list.length} names in the array."
+
+ # names_list.pop
+names_list.each {|the_name| puts "Hello, #{the_name[:name]}"}
